59f9d25dec33e198a994f94e804d86f509c9a8ef
Vishal <vishaltyagi.dev098@gmail.com>
2023-07-02 15:58:18 +0530

    final

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const serverless=require('serverless-http')
 const app = express();
 const fs = require('fs');
+const router=express.Router();
 
 // Read the JSON data file
 const jsonData = JSON.parse(fs.readFileSync('data.json'));
 
 // GET endpoint with filtering options
-app.get('/data', (req, res) => {
+router.get('/data', (req, res) => {
   const filters = req.query;
   const filteredUsers = jsonData.filter(user => {
     let isValid = true;
@@ -19,6 +21,9 @@ app.get('/data', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
+
+app.use('./netlify/functions/api',router);
+module.exports.handler=serverless(app);
